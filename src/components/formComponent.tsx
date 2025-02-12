227ae934aaf97e43e98856c57607567aed8fd0ce
sparrow12345 <m.naser@innopolis.university>
2025-02-12 15:34:24 +0300

    second

--- src/components/formComponent.tsx
+++ src/components/formComponent.tsx
@@ -45,7 +45,7 @@ const FormComponent: React.FC<formProps> = ({ form }) => {
   };
   
   const formatRules = (rulesInput: string) => {
-    const rulesObj: { [key: string]: string| number } = {};
+    const rulesObj: { [key: string]: string | number } | Record<string, never> = {};
   
     rulesInput.split(',').forEach(pair => {
       const [key, value] = pair.split(':').map(str => str.trim());
